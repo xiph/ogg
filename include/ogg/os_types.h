2226c9015a3f05bbcc7f6fe6e4960780668b2bd0
Monty <xiphmont@xiph.org>
2001-06-03 03:09:18 +0000

    Brian was right; I missed this one.
    svn path=/trunk/ogg/; revision=1470

--- include/ogg/os_types.h
+++ include/ogg/os_types.h
@@ -11,7 +11,7 @@
  ********************************************************************
 
  function: #ifdef jail to whip a few platforms into the UNIX ideal.
- last mod: $Id: os_types.h,v 1.6 2001/02/26 03:49:58 xiphmont Exp $
+ last mod: $Id: os_types.h,v 1.7 2001/06/03 03:09:18 xiphmont Exp $
 
  ********************************************************************/
 #ifndef _OS_TYPES_H
@@ -25,50 +25,48 @@
 #define _ogg_free    free
 
 #ifdef _WIN32 
+
 #  ifndef __GNUC__
-
-/* MSVC/Borland */
-typedef __int64 ogg_int64_t;
-typedef __int32 ogg_int32_t;
-typedef unsigned __int32 ogg_uint32_t;
-typedef __int16 ogg_int16_t;
-
+   /* MSVC/Borland */
+   typedef __int64 ogg_int64_t;
+   typedef __int32 ogg_int32_t;
+   typedef unsigned __int32 ogg_uint32_t;
+   typedef __int16 ogg_int16_t;
 #  else
-
-/* Cygwin */
-#include <_G_config.h>
-typedef _G_int64_t ogg_int64_t;
-typedef _G_int32_t ogg_int32_t;
-typedef _G_uint32_t ogg_uint32_t;
-typedef _G_int16_t ogg_int16_t;
-
+   /* Cygwin */
+   #include <_G_config.h>
+   typedef _G_int64_t ogg_int64_t;
+   typedef _G_int32_t ogg_int32_t;
+   typedef _G_uint32_t ogg_uint32_t;
+   typedef _G_int16_t ogg_int16_t;
 #  endif
+
+#elif defined(macintosh)
+
+#  include <sys/types.h>
+   typedef SInt16 ogg_int16_t;
+   typedef SInt32 ogg_int32_t;
+   typedef UInt32 ogg_uint32_t;
+   typedef SInt64 ogg_int64_t;
+
+#elif defined(__BEOS__)
+
+   /* Be */
+#  include <inttypes.h>
+
+#elif defined (__EMX__)
+
+   /* OS/2 GCC */
+   typedef short ogg_int16_t;
+   typedef int ogg_int32_t;
+   typedef unsigned int ogg_uint32_t;
+   typedef long long ogg_int64_t;
+
 #else
 
+#  include <sys/types.h>
+#  include <ogg/config_types.h>
 
-#  ifdef macintosh
-
-#include <sys/types.h>
-
-typedef SInt16 ogg_int16_t;
-typedef SInt32 ogg_int32_t;
-typedef UInt32 ogg_uint32_t;
-typedef SInt64 ogg_int64_t;
-
-#  else
-
-#    ifdef __BEOS__
-
-/* Be */
-#include <inttypes.h>
-
-#    endif
-
-#include <sys/types.h>
-#include <ogg/config_types.h>
-
-#  endif /* macintosh */
-
-#endif  /* _WIN32 */
+#endif
 
 #endif  /* _OS_TYPES_H */

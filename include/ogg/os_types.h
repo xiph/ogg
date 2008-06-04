834caa2fb127dfd098fb81650ef5fbcd3226fe96
ivo <ivo@xiph.org>
2008-06-04 03:27:18 +0000

    Build support for Haiku.  Patch by scottmc at gmail.  Closes #1371.
    svn path=/trunk/ogg/; revision=14997

--- include/ogg/os_types.h
+++ include/ogg/os_types.h
@@ -75,6 +75,16 @@
    typedef u_int32_t ogg_uint32_t;
    typedef int64_t ogg_int64_t;
 
+#elif defined(__HAIKU__)
+
+  /* Haiku */
+#  include <sys/types.h>
+   typedef short ogg_int16_t;
+   typedef unsigned short ogg_uint16_t;
+   typedef int ogg_int32_t;
+   typedef unsigned int ogg_uint32_t;
+   typedef long long ogg_int64_t;
+
 #elif defined(__BEOS__)
 
    /* Be */

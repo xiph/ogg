xiph/ogg

commit 6449883ccacfee276ed9d99fa047342cdc51ab88

Fix iOS/macOS build with current SDKs.
Use <sys/types.h> not <inttypes.h> when building for Apple platforms,
as it works with modular dynamic-framework builds.

Signed-off-by: Ralph Giles <giles@thaumas.net>

include/ogg/os_types.h
@@ -70,7 +70,7 @@
 
 #elif (defined(__APPLE__) && defined(__MACH__)) /* MacOS X Framework build */
 
-#  include <inttypes.h>
+#  include <sys/types.h>
    typedef int16_t ogg_int16_t;
    typedef uint16_t ogg_uint16_t;
    typedef int32_t ogg_int32_t;

commit 1099f6ca4e275c55e72a6baa3dab5cdbeefb261a

os_types: fix unsigned typedefs for MacOS
This effectively reverts f8ce071e1040c766157d630d920d6165d35fe422 which was
probably broken by 6449883ccacfee276ed9d99fa047342cdc51ab88.

include/ogg/os_types.h
@@ -72,11 +72,11 @@
 
 #  include <sys/types.h>
    typedef int16_t ogg_int16_t;
-   typedef uint16_t ogg_uint16_t;
+   typedef u_int16_t ogg_uint16_t;
    typedef int32_t ogg_int32_t;
-   typedef uint32_t ogg_uint32_t;
+   typedef u_int32_t ogg_uint32_t;
    typedef int64_t ogg_int64_t;
-   typedef uint64_t ogg_uint64_t;
+   typedef u_int64_t ogg_uint64_t;
 
 #elif defined(__HAIKU__)
 
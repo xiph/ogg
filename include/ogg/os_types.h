05a006aea7926dc03264ee2156d410de4c0aeca2
Francesco Giancane <francescogiancane8@gmail.com>
2020-12-31 18:56:39 +0100

    os_types.h: Properly define types on macOS
    For more recent macOS builds, <sys/types.h> is not enought to correctly
    define aliased types.
    
    Adding <stdint.h> to include list fixes the issue, and makes building
    other libraries (i.e. vorbis) for macOS again.
    
    Signed-off-by: Francesco Giancane <francescogiancane8@gmail.com>

--- include/ogg/os_types.h
+++ include/ogg/os_types.h
@@ -71,6 +71,7 @@
 #elif (defined(__APPLE__) && defined(__MACH__)) /* MacOS X Framework build */
 
 #  include <sys/types.h>
+#  include <stdint.h>
    typedef int16_t ogg_int16_t;
    typedef u_int16_t ogg_uint16_t;
    typedef int32_t ogg_int32_t;
